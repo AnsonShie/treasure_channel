dd0ed5b78a99d5351a96d9cda84c0601281c9e20
AnsonShie <anson12405@gmail.com>
2022-01-12 18:13:39 +0800

    action after receiving msg only when lock is 1

--- main.ts
+++ main.ts
@@ -7,17 +7,25 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(廣播群組)
     if (鎖 == 1) {
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.showString("Unlock")
         鎖 = 0
     } else {
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.showString("Lock")
         鎖 = 1
     }
     basic.pause(2000)
     basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(100)
-    basic.showIcon(IconNames.SmallHeart)
+    if (鎖 == 1) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     if (鎖 == 0) {
@@ -29,7 +37,7 @@ let 鎖 = 0
 let 廣播群組 = 0
 廣播群組 = 1
 鎖 = 1
-radio.setTransmitPower(3)
+radio.setTransmitPower(0)
 basic.forever(function () {
     radio.setGroup(廣播群組)
     radio.sendString("\"I got you\"")
